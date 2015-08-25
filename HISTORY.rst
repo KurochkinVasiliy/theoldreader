c8eca26cb9f33c00bb55e57eff5c33d452069ec6
the01 <the01@bogus.com>
2015-08-25 05:53:52 +0200

    made python module; added setup.py; rewritten README.md to README.rst; added HISTORY.rst

---
+++ HISTORY.rst
@@ -0,0 +1,9 @@
+.. :changelog:
+
+History
+=======
+
+0.1.0 (2015-08-25)
+------------------
+
+* Create python module file structure
---
+++ theoldreader/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+""" A python wrapper for making calls to the TheOldReader API """
+
+__author__ = "Qra"
+# TODO: added author email
+__email__ = ""
+__copyright__ = "Copyright (C) 2015, Kurochkin Vasiliy"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__date__ = "2015-08-25"
+
+from .tor import Connection, ItemsSearch, Item
